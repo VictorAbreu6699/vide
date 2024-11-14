f6b0bd91022839defb6b9cebded4f7b1217e385d
Victor Abreu Azevedo <azevedovictor6699@gmail.com>
2024-11-14 13:27:10 -0300

    login

--- src/Controllers/AuthController.py
+++ src/Controllers/AuthController.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.templating import Jinja2Templates
 
 from src.Helpers.JWTHelper import JWTHelper
@@ -13,10 +13,18 @@ templates = Jinja2Templates(directory="src/Templates")
 @router.post("/login")
 def login(request: LoginRequest):
     user = UserRepository().get_by_email(request.email)
-    login_validated = JWTHelper().verify_password(user.password, request.password)
+    if user is None:
+        return {"message": "E-mail ou senha incorretos."}
+
+    login_validated = JWTHelper().verify_password(request.password, user.password)
     if not login_validated:
         return {"message": "E-mail ou senha incorretos."}
 
-    token = JWTHelper.create_access_token()
+    access_token = JWTHelper.create_access_token({"sub": user.email})
 
-    return {"message": "Login realizado com sucesso!", "token": token}
+    return {"message": "Login realizado com sucesso!", "access_token": access_token, "token_type": "bearer"}
+
+
+@router.get('/check-login-test', dependencies=[Depends(JWTHelper.validate_token)])
+def test_login():
+    return {'message': 'logado'}
--- src/Helpers/JWTHelper.py
+++ src/Helpers/JWTHelper.py
@@ -1,11 +1,17 @@
 import os
 from datetime import datetime, timedelta
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
 from src.Helpers.CryptHelper import CryptHelper
 import jwt
-
+from dotenv import load_dotenv
 
 class JWTHelper:
+    oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+    load_dotenv()
+
     @staticmethod
     def verify_password(plain_password, hashed_password) -> bool:
         hashed_password = CryptHelper().decrypt(hashed_password)
@@ -15,7 +21,7 @@ class JWTHelper:
     @staticmethod
     def create_access_token(data: dict) -> str:
         to_encode = data.copy()
-        expire = datetime.utcnow() + timedelta(minutes=os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES"))
+        expire = datetime.utcnow() + timedelta(minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 60)))
         to_encode.update({"exp": expire})
         encoded_jwt = jwt.encode(to_encode, os.getenv("JWT_SECRET_KEY"), algorithm="HS256")
         return encoded_jwt
@@ -31,3 +37,19 @@ class JWTHelper:
         except jwt.InvalidTokenError:
             print("Token inválido")
             return None
+
+    @staticmethod
+    def is_user_logged_in(token: str) -> bool:
+        payload = JWTHelper.decode_access_token(token)
+        # Retorna True se o token for válido, caso contrário False
+        return payload is not None
+
+    @staticmethod
+    def validate_token(token: str = Depends(oauth2_scheme)):
+        # Verifica se o usuário está logado
+        if not JWTHelper.is_user_logged_in(token):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token inválido ou expirado",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
