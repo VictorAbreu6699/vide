dece8728cfb42ba7e86c32a9c3c39393e7f7b91b
Victor Abreu Azevedo <azevedovictor6699@gmail.com>
2025-02-04 23:02:12 -0300

    Coluna is_active para usuários

--- src/Models/User.py
+++ src/Models/User.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -10,3 +10,4 @@ class User(Base):
     name = Column(String(255), nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
