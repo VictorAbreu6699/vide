98bda8bcaea9735120ba940830bc74d0cc27c2a4
Victor Abreu Azevedo <azevedovictor6699@gmail.com>
2025-05-21 21:52:37 -0300

    filtro de ano

--- src/Templates/js/show-report/choropleth-map.js
+++ src/Templates/js/show-report/choropleth-map.js
@@ -1,6 +1,6 @@
-function buildChoroplethMap(container_id, data) {
-    buildSelectYear(data)
-    data = groupDataByYear(data)
+function buildChoroplethMap(container_id, report_visualization) {
+    buildSelectYear(report_visualization.filters)
+    data = groupDataByYear(report_visualization.data)
     // Inicializar o mapa usando a biblioteca Leaflet
     const map = L.map(container_id).setView([-14.2350, -51.9253], 4); // Centro aproximado do Brasil
 
@@ -8,15 +8,6 @@ function buildChoroplethMap(container_id, data) {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors',
     }).addTo(map);
-
-    // Dados fictícios (latitude, longitude e informações)
-//    data = [
-//    { lat: -23.55052, lng: -46.633308, name: 'São Paulo', cases: 500 },
-//    { lat: -22.906847, lng: -43.172896, name: 'Rio de Janeiro', cases: 300 },
-//    { lat: -19.916681, lng: -43.934493, name: 'Belo Horizonte', cases: 200 },
-//    { lat: -3.119027, lng: -60.021731, name: 'Manaus', cases: 150 },
-//    { lat: -8.047562, lng: -34.877, name: 'Recife', cases: 120 },
-//    ];
 
     // Adicionar os marcadores no mapa
     data.forEach(row => {
--- src/Templates/js/show-report/filters.js
+++ src/Templates/js/show-report/filters.js
@@ -1,8 +1,13 @@
 function buildSelectYear(data) {
+    data_select = data.years.map(function(item){
+        return {"id": item, "text": item}
+    })
+
     console.log(data)
     $("#filters-div").show()
     $("#filter-year").show()
     $('#select-filter-year').select2({
+        data: data_select,
         language: {
             noResults: function() {
                 return "Nenhum resultado encontrado";
